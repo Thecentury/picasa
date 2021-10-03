a36e46e718f8f2ac4c10d83bf3e35b72e74e361e
Mikhail Brinchuk <thecentury@gmail.com>
2021-10-03 08:53:57 +0200

    Added launcher project

---
+++ PicasaLauncher/AppDelegate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using AppKit;
+using Foundation;
+
+namespace Testy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        private void Log (string s)
+        {
+            using (var fs = new FileStream("/Users/mic/Downloads/picasa.log", FileMode.Append, FileAccess.Write))
+            using (var writer = new StreamWriter(fs))
+            {
+                writer.WriteLine($"{DateTime.Now.ToString(CultureInfo.InvariantCulture)} {s}");
+                writer.Flush();
+            }
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Log("DidFinishLaunching");
+            // Insert code here to initialize your application
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            Log("WillTerminate");
+            // Insert code here to tear down your application
+        }
+
+        [Export("application:openFile:")]
+        public override bool OpenFile(NSApplication sender, string filename)
+        {
+            Log($"Open file '{filename}'");
+            return true;
+        }
+    }
+}
