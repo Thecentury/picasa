7f3a0dc15b929be3bb685c0f45dc7124c697b841
thecentury <thecentury@gmail.com>
2025-06-15 19:08:58 +0200

    Fixed the deletion of files to the trash bin

--- Picasa.Core/Services.fs
+++ Picasa.Core/Services.fs
@@ -7,16 +7,30 @@ open Picasa.Model
 open Picasa.Prelude // For Path type
 open Serilog
 
+/// Function to escape single quotes within a string for AppleScript literal usage.
+/// In AppleScript, a single quote inside a double-quoted string literal is escaped as \'
+let private escapeForAppleScript (s: string) =
+    s.Replace("'", "\\'")
+
 let private deleteImageImpl (Path path) = async {
     if RuntimeInformation.IsOSPlatform(OSPlatform.OSX) then
         Log.Debug $"Deleting '{path}'"
-        use p = Process.Start("osascript", $"-e \"tell app \\\"Finder\\\" to move the POSIX file \\\"{path}\\\" to trash\"")
+        let escapedFilePath = escapeForAppleScript (Path.GetFullPath path)
+        let startInfo = ProcessStartInfo("osascript")
+        startInfo.ArgumentList.Add("-e")
+        startInfo.ArgumentList.Add($"""tell app "Finder" to move {{the POSIX file "{escapedFilePath}"}} to trash""")
+        startInfo.RedirectStandardError <- true
+        startInfo.RedirectStandardOutput <- true
+        startInfo.UseShellExecute <- false
+        use p = Process.Start(startInfo)
         do! p.WaitForExitAsync () |> Async.AwaitTask
         let exitCode = p.ExitCode
         if exitCode = 0 then
             return Ok ()
         else
-            Log.Warning $"Deleting '{path}': Exit code was not 0: {exitCode}"
+            let output = p.StandardOutput.ReadToEnd()
+            let errorOutput = p.StandardError.ReadToEnd()
+            Log.Warning $"Deleting '{path}': Exit code was not 0: {exitCode}. Output: {output}. Args: [{startInfo.Arguments}], error: {errorOutput}"
             return Error "Failed to delete the image"
     else
         return Error "Delete not implemented for this platform"
